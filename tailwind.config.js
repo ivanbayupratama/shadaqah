99d00da39167b61704f3fad3fc25ca15f109f4b1
frking69 <oding.223040081@mail.unpas.ac.id>
2024-12-20 17:00:53 +0700

    Membuat Halaman Admin Dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        
     ],
     theme: {
         extend: {
@@ -16,5 +17,7 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 };
