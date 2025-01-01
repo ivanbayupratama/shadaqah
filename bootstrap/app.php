7da0e805ce71fd2cd2da0a352996085b9cff23bf
Ivan Bayu <you@example.com>
2025-01-01 15:41:23 +0700

    Resolve merge conflict and update files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,24 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        // Menggunakan metode 'append' untuk menambahkan middleware
+    ->withMiddleware(callback: function (Middleware $middleware): void {
+        // Menggunakan metode 'append' untuk menambahkan middleware dari branch `dev-campaignFeature`
         $middleware->append(\App\Http\Middleware\VerifyCsrfToken::class);
+
+        // Menggunakan middleware group dari branch `Dev`
+        $middleware->group('api', [
+            Illuminate\Routing\Middleware\SubstituteBindings::class,
+            App\Http\Middleware\EnsureJsonResponse::class, // Middleware untuk memastikan respons JSON
+        ]);
+
+        $middleware->group('web', [
+            Illuminate\Session\Middleware\StartSession::class,
+            Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
