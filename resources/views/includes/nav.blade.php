ccaa4e2d46bf12dca6cd812f7dfbcabe10c1490d
RaihanNagata <Raihan.223040054@mail.unpas.ac.id>
2025-01-02 14:52:05 +0700

    perbaikan untuk footer

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -54,16 +54,16 @@
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                     <a href="{{ route('home') }}"
-                        class="block py-2 px-3 @if (Route::is('home')) bg-blue-700 md:bg-transparent text-white md:text-blue-700 underline @else text-white @endif rounded hover:bg-blue-500 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                        class="block py-2 px-3 @if (Route::is('home')) bg-blue-700 md:bg-transparent  md:text-blue-700 underline @else text-white @endif rounded hover:bg-blue-500 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                         aria-current="page">Home</a>
                 </li>
                 <li>
                     <a href="{{ route('about') }}"
-                        class="block py-2 px-3 text-white @if (Route::is('about')) bg-blue-700 md:bg-transparent text-white md:text-blue-700 underline @else text-white @endif rounded hover:bg-blue-500 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Tentang Kami</a>
+                        class="block py-2 px-3  @if (Route::is('about')) bg-blue-700 md:bg-transparent  md:text-blue-700 underline @else text-white @endif rounded hover:bg-blue-500 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Tentang Kami</a>
                 </li>
                 <li>
                     <a href="{{ route('contact') }}"
-                        class="block py-2 px-3 text-white @if (Route::is('contact')) bg-blue-700 md:bg-transparent text-white md:text-blue-700 underline @else text-white @endif rounded hover:bg-blue-500 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact Us</a>
+                        class="block py-2 px-3  @if (Route::is('contact')) bg-blue-700 md:bg-transparent  md:text-blue-700 underline @else text-white @endif rounded hover:bg-blue-500 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact Us</a>
                 </li>
             </ul>
             
